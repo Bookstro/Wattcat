e3f4d82309a3736133faa85574ec47d448e123a0
dgknca <dgknca@gmail.com>
2019-03-12 00:07:31 +0300

    okuma sayfası eklendi

---
+++ javascript/okuma.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+$(".gece_modu_icon").click(function(){
+    $("body").toggleClass("gece_modu");
+    $(".ilerleme_yazi").toggleClass("basliklar_toggle")
+    $(".baslik").toggleClass("basliklar_toggle")
+});
+
+
+
+
+});
